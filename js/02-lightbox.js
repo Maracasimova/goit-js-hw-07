87d20d122693b817771d52714f80585e52ca9d0c
Casimova Marina <maracasimova@gmail.com>
2023-02-28 20:01:32 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      `;
+    })
+    .join("");
+}
+
+const lightBox = new SimpleLightBox(".gallery a", {
+  overlayOpacity: 0.9,
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 250,
+});
